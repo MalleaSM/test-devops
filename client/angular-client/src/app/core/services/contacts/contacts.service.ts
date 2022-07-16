ae979df9d2c6c1fa78e5073993e153d01f4f3162
Sergio Mallea <sergiomallea80@gmail.com>
2022-07-16 01:26:35 -0400

    Update contacts.service.ts
    Added and modify new services to contact:
     _getContactById needs a number parameter
    _added services to send messages and check the messages sent

--- client/angular-client/src/app/core/services/contacts/contacts.service.ts
+++ client/angular-client/src/app/core/services/contacts/contacts.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 
 import IContact from '@shared/models/contact.model';
 import { API_SERVICE_URL } from '@env';
+import IMessage from '@shared/models/message.model';
 
 /**
  * Service for Contacts feature
@@ -39,12 +40,26 @@ export class ContactsService {
    * @param {string} contactId - the contact id attribute
    * @returns {Observable}
    */
-  getContactById(contactId: string): Observable<IContact> {
-    return this.http.get<IContact>(`${API_SERVICE_URL}/contacts/${contactId}`);
+  getContactById(contactId: number): Observable<IContact> {
+    return this.http.get<IContact>(`${API_SERVICE_URL}/contact/${contactId}`);
   }
 
   getContactByEmail(contactEmail: string, id:number){
     return this.http.get<IContact>(`${API_SERVICE_URL}/contact/${id}/exists/${contactEmail}`)
   }
+
+    /**
+   * Send a message
+   * @param {IMessage} message - Contact Object
+   * @returns {Observable}
+   */
+  sendMessageToContact(message: IMessage): Observable<IMessage> {
+    return this.http.post<IMessage>(`${API_SERVICE_URL}/message`, message);
+  }
+
+  sentMessages(sendId:number){
+    return this.http.get<IContact>(`${API_SERVICE_URL}/message/sender/${sendId}`)
+  }
+
 
 }
